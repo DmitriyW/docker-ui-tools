b32e65a2cdb0833c12620188c53fefcf02e35b57
Дмитрий Белоконь <dmitriy.belokon@gmail.com>
2023-10-15 01:04:17 +0300

    make method as async.

--- DockerUITools/Docker.Services/ContainerServices.cs
+++ DockerUITools/Docker.Services/ContainerServices.cs
@@ -7,47 +7,66 @@ public class ContainerServices
 {
     public ContainerServices() { }
 
-    public IEnumerable<Container> GetContainers()
+    public async Task GetContainersAsync(IProgress<IEnumerable<Container>> progress)
     {
-        var result = CommandRunner
+        await Task.Run(() =>
+        {
+            var result = CommandRunner
             .RunCommand("docker", "ps -a --format json")
             .ToString()
             .SplitToRows();
 
-        var jsonHelper = new JsonHelper<Container>();
-
-        var containers = jsonHelper.ConvertTo(result);
-
-        return containers;
+            var jsonHelper = new JsonHelper<Container>();
+            var containers = jsonHelper.ConvertTo(result);
+            progress.Report(containers);
+        });
     }
 
-    public string StartContainer(string containerId)
-        => CommandRunner
-            .RunCommand("docker", $"start {containerId}")
-            .ToString();
+    public async Task StartContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                    .RunCommand("docker", $"start {containerId}")
+                    .ToString());
+            });
 
-    public string RestartContainer(string containerId)
-        => CommandRunner
-            .RunCommand("docker", $"restart {containerId}")
-            .ToString();
+    public async Task RestartContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                    .RunCommand("docker", $"restart {containerId}")
+                    .ToString());
+            });
 
-    public string StopContainer(string containerId)
-        => CommandRunner
-            .RunCommand("docker", $"stop {containerId}")
-            .ToString();
+    public async Task StopContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                        .RunCommand("docker", $"stop {containerId}")
+                        .ToString());
+            });
 
-    public string PauseContainer(string containerId)
-        => CommandRunner
-            .RunCommand("docker", $"pause {containerId}")
-            .ToString();
+    public async Task PauseContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                        .RunCommand("docker", $"pause {containerId}")
+                        .ToString());
+            });
 
-    public string UnpauseContainer(string containerId)
-        => CommandRunner
-            .RunCommand("docker", $"unpause {containerId}")
-            .ToString();
+    public async Task UnpauseContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                        .RunCommand("docker", $"unpause {containerId}")
+                        .ToString());
+            });
 
-    public string DeleteContainer(string containerId)
-    => CommandRunner
-        .RunCommand("docker", $"rm {containerId}")
-        .ToString();
+    public async Task DeleteContainerAsync(IProgress<string> progress, string containerId)
+        => await Task.Run(() =>
+            {
+                progress.Report(CommandRunner
+                        .RunCommand("docker", $"rm {containerId}")
+                        .ToString());
+            });
 }
